bb24c51d11b9365a93636c1aab698ec133fa87fc
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-03 12:19:41 -0300

    feat: add UserButton to header for user authentication display

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import { AppSidebar } from "../_components/app-sidebar";
 import {
   SidebarInset,
@@ -14,9 +15,12 @@ export default function RootLayout({
     <SidebarProvider>
       <AppSidebar className="bg-black text-white" />
       <SidebarInset>
-        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
-          <SidebarTrigger className="-ml-1" />
-          <h1>FormoTech - Arniqueiras</h1>
+        <header className="sticky top-0 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-background px-4">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <h1>FormoTech - Arniqueiras</h1>
+          </div>
+          <UserButton showName />
         </header>
         <div className="flex flex-1 flex-col gap-4 bg-slate-200 p-4">
           {children}
